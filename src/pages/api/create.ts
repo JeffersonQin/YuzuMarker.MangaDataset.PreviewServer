0c9096baa1d27b72892491151cafbe602344463d
gyro永不抽风 <1247006353@qq.com>
2023-04-15 03:08:30 -0400

    feat: seperate logic

---
+++ src/pages/api/create.ts
@@ -0,0 +1,61 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+
+const { API_KEY } = process.env;
+
+type Data = {
+  uuid: string;
+  msg: string;
+};
+
+function createFolder(folderPath: string) {
+  const folderUUID = uuidv4();
+  folderPath = `${folderPath}/${folderUUID}`;
+  fs.mkdirSync(folderPath, { recursive: true }); // create folder if it doesn't exist
+  return folderUUID;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "POST") {
+    const { key } = await req.body;
+    if (key !== API_KEY) {
+      res.status(401).json({ uuid: "", msg: "invalid key" });
+      return;
+    }
+    try {
+      const folderUUID = createFolder(`public/images`);
+
+      // Start the task with a delay of 3 days
+      setTimeout(async () => {
+        try {
+          await fs.promises.rmdir(`public/images/${folderUUID}`, {
+            recursive: true,
+          });
+          console.log(`Preview ${folderUUID} deleted successfully`);
+        } catch (err) {
+          console.error("Error deleting files:", err);
+        }
+      }, 259200000); // 3 days in milliseconds
+
+      res.status(200).json({ uuid: folderUUID, msg: "success" });
+    } catch (err) {
+      console.error("Error deleting files:", err);
+      res.status(500).json({ uuid: "", msg: `error: ${err}` });
+    }
+  } else {
+    res.status(404).json({ uuid: "", msg: "use POST to send message" });
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "1000mb",
+    },
+  },
+};
--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -6,23 +6,15 @@ import { v4 as uuidv4 } from "uuid";
 const { API_KEY } = process.env;
 
 type Data = {
-  uuid: string;
   msg: string;
 };
 
 function saveImages(
-  images: { ext: string; content: string }[],
+  image: { ext: string; content: string; index: number },
   folderPath: string
-): string {
-  const folderUUID = uuidv4();
-  folderPath = `${folderPath}/${folderUUID}`;
-  fs.mkdirSync(folderPath, { recursive: true }); // create folder if it doesn't exist
-
-  Array.from(images.entries()).forEach(([index, image]) => {
-    const filepath = `${folderPath}/${index}.${image.ext}`;
-    fs.writeFileSync(filepath, Buffer.from(image.content, "base64"));
-  });
-  return folderUUID;
+): void {
+  const filepath = `${folderPath}/${image.index}.${image.ext}`;
+  fs.writeFileSync(filepath, Buffer.from(image.content, "base64"));
 }
 
 export default async function handler(
@@ -30,28 +22,20 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method === "POST") {
-    const { images, key } = await req.body;
+    const { image, uuid, key } = await req.body;
     if (key !== API_KEY) {
-      res.status(401).json({ uuid: "", msg: "invalid key" });
+      res.status(401).json({ msg: "invalid key" });
       return;
     }
-    const folderUUID = saveImages(images, "public/images");
-
-    // Start the task with a delay of 3 days
-    setTimeout(async () => {
-      try {
-        await fs.promises.rmdir(`public/images/${folderUUID}`, {
-          recursive: true,
-        });
-        console.log(`Preview ${folderUUID} deleted successfully`);
-      } catch (err) {
-        console.error("Error deleting files:", err);
-      }
-    }, 259200000); // 3 days in milliseconds
-
-    res.status(200).json({ uuid: folderUUID, msg: "success" });
+    try {
+      saveImages(image, `public/images/${uuid}`);
+      res.status(200).json({ msg: "success" });
+    } catch (err) {
+      console.error("Error deleting files:", err);
+      res.status(500).json({ msg: `error: ${err}` });
+    }
   } else {
-    res.status(404).json({ uuid: "", msg: "use POST to send message" });
+    res.status(404).json({ msg: "use POST to send message" });
   }
 }
 
